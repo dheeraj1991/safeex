be1920991452ff12f31882620499e13a3a6dda7e
Dheeraj Prasanth <dheeraj.prasanth@gmail.com>
2021-12-06 14:58:44 +0530

    Spelling mistakes corrected

--- app/create_user.py
+++ app/create_user.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 if not User.objects.filter(username='dheeraj.prasanth@gmail.com').exists():
     User.objects.create_superuser('dheeraj.prasanth@gmail.com', 'dheeraj.prasanth@gmail.com', 'jango@10')
     print("Super User Added")
-user, created = User.objects.get_or_create(username='vasilii@safeex.com', email='vasilii@safeex.com')
+user, created = User.objects.get_or_create(username='vasili@safeex.com', email='vasili@safeex.com')
 if created:
     user.set_password('testpass123')
     user.save()
